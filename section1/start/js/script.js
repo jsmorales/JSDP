806835de7bed9c5184c60195b4868edb7cd1cc98
jsmorales <jhellmetal2000@gmail.com>
2017-01-16 23:15:53 -0500

    Se pasa la leccion 1 creando unas funciones publicas para anejo de unos objetivos.

--- section1/start/js/script.js
+++ section1/start/js/script.js
@@ -9,7 +9,46 @@ var leadself = 'Me: ',
 									"She laughed, and the desert sang.",
 									"You’ve got about as much charm as a dead slug."];
 
+function talk(msg) {	
+	echo(leadself + msg)
+}
 
+function replayYesNo() {
+	/*Para decir si o no se usa una variable
+	random la cual si es mayor a 5 dice que si
+	*/
+	var msg = Math.random()>.5 ? msgYes : msgNo
+
+	echo(leadcomputer + msg)
+}
+
+function saySassyStuf() {
+	/*se selecciona un item al azar y se muestra
+	con la premisa leadcomputer
+	*/
+	var msg = aSassyStuff[ Math.floor(Math.random()*aSassyStuff.length) ]
+	echo(leadcomputer + msg)
+}
+
+function echo(msg) {
+	//contiene el mensaje en un array como 
+	//una capa.
+	aSaid.push( "<div>" + msg + "</div>")
+	//pone el texto en en div
+	$("#talk span").text(msg)
+
+	//setea las variables, para que saque el maximo 
+	//de la variable aSaid y muestre todo lo que
+	//hay dentro pero no mas de 6 mensajes.
+	var start = Math.max(aSaid.length - 6,0),
+		out = ""
+	
+	for (var i = start; i < aSaid.length; i++) {
+		out += aSaid[i]; 
+	}
+
+	$(".advert").html(out)
+}
 
 
 
