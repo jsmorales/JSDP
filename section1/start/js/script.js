80dfe916584a469e0f2b8153135e5c59fac44231
jsmorales <jhellmetal2000@gmail.com>
2017-01-19 22:37:17 -0500

    Se adjuntan todas las funciones en un patron de diseño basico en un objeto.

--- section1/start/js/script.js
+++ section1/start/js/script.js
@@ -1,57 +1,52 @@
-var leadself = 'Me: ',
-		leadcomputer = "PC: ",
-		aSaid= ["This is a Cyber Chat"],
-		msgYes = "Yes, that's a great idea.",
-		msgNo = "No, that must be a mistake.",
-		aSassyStuff = ["Like mold on books, grow myths on history.",
-									"She moved like a poem and smiled like a sphinx.",
-									"As long as we don’t die, this is gonna be one hell of a story.",
-									"She laughed, and the desert sang.",
-									"You’ve got about as much charm as a dead slug."];
+var o = {
+	leadself : 'Me: ',
+	leadcomputer : "PC: ",
+	aSaid: ["This is a Cyber Chat"],
+	msgYes : "Yes, that's a great idea.",
+	msgNo : "No, that must be a mistake.",
+	aSassyStuff : ["Like mold on books, grow myths on history.",
+					"She moved like a poem and smiled like a sphinx.",
+					"As long as we don’t die, this is gonna be one hell of a story.",
+					"She laughed, and the desert sang.",
+					"You’ve got about as much charm as a dead slug."],
+	talk: function (msg) {	
+		this.echo(this.leadself + msg)
+	},
+	replayYesNo: function () {
+		/*Para decir si o no se usa una variable
+		random la cual si es mayor a 5 dice que si
+		*/
+		var msg = Math.random()>.5 ? this.msgYes : this.msgNo
 
-function talk(msg) {	
-	echo(leadself + msg)
-}
+		this.echo(this.leadcomputer + msg)
+	},
 
-function replayYesNo() {
-	/*Para decir si o no se usa una variable
-	random la cual si es mayor a 5 dice que si
-	*/
-	var msg = Math.random()>.5 ? msgYes : msgNo
+	saySassyStuf: function () {
+		/*se selecciona un item al azar y se muestra
+		con la premisa leadcomputer
+		*/
+		var msg = this.aSassyStuff[ Math.floor(Math.random()*this.aSassyStuff.length) ]
+		echo(this.leadcomputer + msg)
+	},
 
-	echo(leadcomputer + msg)
-}
+	echo: function (msg) {
+		//contiene el mensaje en un array como 
+		//una capa.
+		this.aSaid.push( "<div>" + msg + "</div>")
+		//pone el texto en en div
+		$("#talk span").text(msg)
 
-function saySassyStuf() {
-	/*se selecciona un item al azar y se muestra
-	con la premisa leadcomputer
-	*/
-	var msg = aSassyStuff[ Math.floor(Math.random()*aSassyStuff.length) ]
-	echo(leadcomputer + msg)
-}
+		//setea las variables, para que saque el maximo 
+		//de la variable aSaid y muestre todo lo que
+		//hay dentro pero no mas de 6 mensajes.
+		var start = Math.max(this.aSaid.length - 6,0),
+			out = ""
 
-function echo(msg) {
-	//contiene el mensaje en un array como 
-	//una capa.
-	aSaid.push( "<div>" + msg + "</div>")
-	//pone el texto en en div
-	$("#talk span").text(msg)
+		for (var i = start; i < this.aSaid.length; i++) {
+			out += this.aSaid[i]; 
+		}
 
-	//setea las variables, para que saque el maximo 
-	//de la variable aSaid y muestre todo lo que
-	//hay dentro pero no mas de 6 mensajes.
-	var start = Math.max(aSaid.length - 6,0),
-		out = ""
-	
-	for (var i = start; i < aSaid.length; i++) {
-		out += aSaid[i]; 
+		$(".advert").html(out)
 	}
 
-	$(".advert").html(out)
 }
-
-
-
-
-
-
